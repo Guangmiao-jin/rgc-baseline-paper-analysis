2ccdc73f2efeff6adc0b260bcf740f6626ec3909
Guangmiao-jin <jinguangmiao@gmail.com>
2026-05-27 10:54:34 +0100

    Remove the outdated processing pipeline

--- HS2_processing/V1/dispProgressMsg.m
+++
@@ -1,9 +0,0 @@
-function dispProgressMsg(msg)
-ASCII_BKSP_CHAR = 8;
-persistent prevMsgLen;
-if isempty(prevMsgLen) 
-    prevMsgLen = 0;
-end
-    
-disp([ char(repmat(ASCII_BKSP_CHAR,1,prevMsgLen)) msg]);
-prevMsgLen = numel(msg)+1;
--- HS2_processing/V1/ensureColumnNumeric.m
+++
@@ -1,9 +0,0 @@
- function x = ensureColumnNumeric(x)
-        if isempty(x)
-            x = [];
-        elseif ~isnumeric(x)
-            x = [];
-        else
-            x = x(:);  % force column vector
-        end
- end
